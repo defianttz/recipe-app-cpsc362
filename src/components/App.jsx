65aece1cdec74c4c557f9140985c5da6700dc51f
defianttz <80181373+defianttz@users.noreply.github.com>
2022-03-09 22:23:36 -0800

    Initial Repo

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Header from "./Header";
+
+function App() {
+ 
+  
+
+  return (
+    <div>
+      <Header />
+      
+    </div>
+  );
+}
+
+export default App;
