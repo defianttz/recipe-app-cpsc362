f440f53b7753d1b95722e46b6119ae9df69374e6
keiwenhuang <kaoghk@gmail.com>
2022-04-11 21:47:18 -0700

    API calls

--- src/components/recipespace/RecipeSpace.jsx
+++ src/components/recipespace/RecipeSpace.jsx
@@ -6,10 +6,29 @@ import RecipeCard from "../recipecard/RecipeCard";
 //import RecipeInfoCard from "../recipeinfo/RecipeInfoCard";
 
 import mockdata from "./mockdata.js";
+import axios from "axios";
 
 const Recipespace = (props) => {
-  //const apiURL = `https://api.spoonacular.com/recipes/${id}/information`;
-  //const apiKey = "34ac49879bd04719b7a984caaa4006b4";
+  const apiURL = 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com';
+  const apiKey = "4l2VUZDPcFmshNG9ypRwW0gs8XFqp1rk4dajsnVnpEyMHEyIWU";
+
+  const options = {
+    method: 'GET',
+    url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch',
+    params: {addRecipeInformation: 'true', 
+            query: props.searchTerm,
+            addRecipeNutrition: 'true'},
+    headers: {
+      'X-RapidAPI-Host': apiURL,
+      'X-RapidAPI-Key': apiKey
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+    console.log(response.data.results);
+  }).catch(function (error) {
+    console.error(error);
+  });
 
   const [recipeData, setRecipeData] = useState(mockdata);
   console.log("RecipeSpace:searchTerm " + props.searchTerm);
