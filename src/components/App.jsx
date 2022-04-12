247b223386c08054ef20ac7e6aaad3a9df796ab3
A-Hoblit <Hoblit.Andrew@gmail.com>
2022-04-11 21:55:17 -0700

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "./footer/Footer";
 //import SearchAppBar from "./Header";
 //import Grid from "@mui/material/Grid";
